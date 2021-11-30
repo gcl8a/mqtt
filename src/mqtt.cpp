d74ed5be1f8c4648fb8b937125bf35abe1f7c28a
Greg <lewin.gc@gmail.com>
2021-11-30 07:03:26 -0500

    initial commit

---
+++ src/mqtt.cpp
@@ -0,0 +1,100 @@
+#include <mqtt.h>
+#include <wifi_credentials.h>
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+const uint32_t KEEP_ALIVE_INTERVAL = 20;
+
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println(WiFi.macAddress());
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+uint32_t lastWiFiCxnAttempt = 0;
+uint32_t wifiCxnRetryInterval = 10000;
+
+void wifi_reconnect(void)
+{
+  if(WiFi.status() != WL_CONNECTED)
+  {
+    if(millis() - lastWiFiCxnAttempt > wifiCxnRetryInterval)
+    {
+      lastWiFiCxnAttempt = millis();
+      WiFi.begin(ssid, password);
+      
+      randomSeed(micros());
+    }
+  }
+}
+
+uint32_t lastCxnAttempt = 0;
+uint32_t cxnRetryInterval = 1500;
+
+void reconnect() 
+{
+  wifi_reconnect();
+
+  //try to reconnect once
+  while(!client.connected()) 
+  {
+    if(millis() - lastCxnAttempt > cxnRetryInterval)
+    {
+      lastCxnAttempt = millis();
+
+      Serial.print("MQTT cxn...");
+      
+      // Create a random client ID
+      String clientId = "ESP32Client-";
+      clientId += String(random(0xffff), HEX);
+      
+      // Attempt to connect
+      if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) 
+      {
+        Serial.println("connected");
+      } 
+
+      else 
+      {
+        Serial.print("failed, rc = ");
+        Serial.print(client.state());
+        Serial.println("; will try again");
+      }
+    }
+  }
+}
+
+void callback(char* topic, byte *payload, unsigned int length) 
+{
+    Serial.println(topic);
+    Serial.print(':');  
+    Serial.write(payload, length);
+    Serial.println();
+}
+
+void setup_mqtt() 
+{
+    Serial.println("setup_mqtt");
+
+    setup_wifi();
+    client.setServer(mqtt_server, mqtt_port);
+    //client.setCallback(callback);
+    client.setKeepAlive(KEEP_ALIVE_INTERVAL);
+    reconnect();
+
+    Serial.println("/setup_mqtt");
+}
---
+++ src/mqtt.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>  //ESP8266 Core WiFi Library
+#elif defined(ESP32)
+#include <WiFi.h>         //ESP32 Core WiFi Library
+#else
+#include <WiFi.h>
+#endif
+
+#include <PubSubClient.h>
+
+void setup_wifi();
+void wifi_reconnect(void);
+void reconnect();
+void callback(char* topic, byte *payload, unsigned int length);
+void setup_mqtt();
+
+extern WiFiClient wifiClient;
+extern PubSubClient client;
